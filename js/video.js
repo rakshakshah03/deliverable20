b7ee7ef94ae1c0bcac174b36de3bd9aa8c5531f0
rakshakshah03 <125766316+rakshakshah03@users.noreply.github.com>
2023-11-05 23:32:23 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -2,10 +2,19 @@ var video;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.loop = false;
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+ console.log("Play Video");
+ video.play();
+
+ var vol = video.volume * 100;
+ console.log("Volume is: ", vol);
+ document.querySelector("#volume").textContent = vol + "%";
+});
+
 
