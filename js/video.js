31570bdeb4fbb2a6e6b66a1e5161e06ad9d036a6
rakshakshah03 <125766316+rakshakshah03@users.noreply.github.com>
2023-11-06 00:01:21 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -5,9 +5,6 @@ window.addEventListener("load", function() {
 	video = document.querySelector("#player1");
 	video.autoplay = false;
 	video.loop = false;
-	var vid = document.createElement('script');
-	vid.src = 'js/video.js';
-	document.head.appendChild(vid);
 
 });
 
